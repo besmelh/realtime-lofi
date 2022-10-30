a0e038f6af09357dc008e1f6897e0d38f853b4e2
Besmelh Alshaalan <besmelh.s@gmail.com>
2022-10-30 00:15:30 -0400

    initialize weather api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin: 50px 0;
+  width: 80%;
+  max-width: 1200px;
+`;
+
+const SearchBar = styled.input`
+  width: 400px;
+  padding: 20px;
+  border-radius: 30px;
+  border: none;
+`;
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+
+  const units = 'metric';
+  // const units = 'imperial';
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`;
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Container>
+        <SearchBar
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+          type='text'
+        ></SearchBar>
+        <h2>The weather in your location is:</h2>
+        {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+        {data.weather ? <h1>{data.weather[0].main}</h1> : null}
+      </Container>
     </div>
   );
 }
