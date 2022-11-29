08152b872e5806cab52054a198af10dcb0bdc78c
Besmelh Alshaalan <besmelh.s@gmail.com>
2022-11-29 14:44:04 -0500

    rotating box

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,35 @@
 import { Canvas } from '@react-three/fiber';
-import { Stats } from '@react-three/drei';
+import { OrthographicCamera, Stats } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
+
+const AnimatedBox = () => {
+  const meshRef = useRef(null);
+
+  useFrame(() => {
+    console.log('hi');
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+    }
+  });
+
+  return (
+    <mesh ref={meshRef} scale={[1.5, 1.5, 1.5]}>
+      <boxGeometry />
+      <meshStandardMaterial />
+    </mesh>
+  );
+};
 
 export default function Home() {
   return (
     <div className='container'>
       <Canvas>
+        <OrthographicCamera />
         <Stats />
         <ambientLight intensity={0.1} />
         <directionalLight color='red' position={[0, 0, 5]} />
-        <mesh>
-          <boxGeometry />
-          <meshStandardMaterial />
-        </mesh>
+        <AnimatedBox />
       </Canvas>
     </div>
   );
